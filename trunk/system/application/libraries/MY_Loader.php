3d8c9a6e470781cfac5816b5874ccd33fa3d6574
Zach Ploskey <zploskey@gmail.com>
2009-01-17 03:14:05 +0000

    Major UI and backend tweaks.
    git-svn-id: svn+ssh://ovid.u.washington.edu/rc22/d09/cosmolab/repos/svn/chem_db@28 b159389c-fc5d-0410-935c-b53355241f03

---
+++ trunk/system/application/libraries/MY_Loader.php
@@ -0,0 +1,65 @@
+<?php
+class MY_Loader extends CI_Loader{
+	/**
+	 * @addtogroup IgnitedRecord
+	 * @{
+	 */
+	/* 
+	 * Copyright (c) 2008, Martin Wernstahl
+     * All rights reserved.
+     *
+     * Redistribution and use in source and binary forms, with or without
+     * modification, are permitted provided that the following conditions are met:
+     *     * Redistributions of source code must retain the above copyright
+     *       notice, this list of conditions and the following disclaimer.
+     *     * Redistributions in binary form must reproduce the above copyright
+     *       notice, this list of conditions and the following disclaimer in the
+     *       documentation and/or other materials provided with the distribution.
+     *     * The name of Martin Wernstahl may not be used to endorse or promote products
+     *       derived from this software without specific prior written permission.
+     *
+     * THIS SOFTWARE IS PROVIDED BY Martin Wernstahl ``AS IS'' AND ANY
+     * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+     * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+     * DISCLAIMED. IN NO EVENT SHALL Martin Wernstahl BE LIABLE FOR ANY
+     * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+     * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+     * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+     * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+     * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+     * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+     */
+	/**
+	 * Loads the IgnitedRecord class and the Model class, also acts as a wrapper for Loader::model().
+	 * 
+	 * This enables an other way to instantiate IgnitedRecord's child classes:
+	 * @code
+	 * $this->load->ORM();
+	 * $this->load->model('a_child_class');
+	 * // or as a wrapper for model()
+	 * $this->load->ORM('a_child_class','pages');
+	 * @endcode
+	 * 
+	 * @param $model The model name of the model to load, if false ORM() will not load any derived classes (models)
+	 * @param $name The name parameter is passed to the model() function, determining if the property name should differ from the default
+	 * @param $db_conn The database connection passed to the model() function
+	 * 
+	 * @todo Check if db is loaded
+	 */
+	function ORM($model = false, $name = '', $db_conn = FALSE){
+		if(!class_exists('IgnitedRecord'))
+		{
+			// change this line if the IgnitedRecord file is stored elsewhere
+			require_once(APPPATH.'libraries/ignitedrecord/ignitedrecord.php');
+		}
+		
+		if($model != false)
+		{
+			$this->model($model, $name, $db_conn);
+		}
+	}
+	/**
+	 * @}
+	 */
+}
+?>
--- trunk/system/application/views/projects/edit.php
+++ trunk/system/application/views/projects/edit.php
@@ -1,12 +1,16 @@
-<?=anchor('welcome','Main')?> | <?=anchor('projects/list','Projects')?>
+<?=print_r($project)?>
+<?=anchor('welcome','Main')?> | <?=anchor('projects','Projects')?>
 <p><h2>Editing <?=$project->name?>, ID=<?=$project->id?></h2></p>
 
 <p><?php echo validation_errors(); ?></p>
 
-<?=form_open('projects')?>
-<p\>
-Name: 
-<input type="text" name="name" value="<?=$project->name?>" size="50">
-<input type="submit" value="Submit">
+<?=form_open('projects/edit_action')?>
+
+<?=form_hidden('id', $this->uri->segment(3)) ?>
+<?=form_hidden('submit', true) ?>
+
+<p>Name: <input type="text" name="name" value="<?=$project->name?>" size="50" />
+
+<input type="submit" value="Submit" />
 </form>
 <p\>
