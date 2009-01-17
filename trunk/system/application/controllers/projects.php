08143ac269a4d45a67be0e4a64780b6c82d8f8d1
Zach Ploskey <zploskey@gmail.com>
2009-01-17 03:14:05 +0000

    Major UI and backend tweaks.
    git-svn-id: svn+ssh://ovid.u.washington.edu/rc22/d09/cosmolab/repos/svn/chem_db@28 b159389c-fc5d-0410-935c-b53355241f03

--- trunk/system/application/controllers/projects.php
+++ trunk/system/application/controllers/projects.php
@@ -1,32 +1,127 @@
 <?php
 
-class Projects extends Controller {
-
-	function Projects() {
+class Projects extends Controller
+{
+	
+	/**
+	 * Contructs object, connects to model, and loads necessary libraries.
+	 * 
+	 **/
+	function Projects()
+	{
 		parent::Controller();
-		$this->load->model('project');
+		$this->load->model('project');	
+		$this->load->library('form_validation', 'session');
 	}
-	
-	function index() {
-		$data['projects'] = $this->project->get_all();
-		$this->load->view('projects/list', $data);
+
+	/**
+	 * Loads page listing projects.
+	 *
+	 * @return void
+	 **/
+	function index()
+	{
+		$data = array(
+			'projects'  => $this->project->read_all(),
+			'title'    => 'Manage Projects',
+			'main'     => 'projects/index'
+		);
+
+		$this->load->view('template', $data);
 	}
-	
-	function edit($id = null) {
-		if ($id != null) {
-			$data['id'] = $id;
-			$this->load->view('projects/edit', $data);
+
+	/**
+	 * 
+	 *
+	 * @return void
+	 **/
+	function add() 
+	{
+		$this->_validate_project();
+		
+		if ($this->form_validation->run() == FALSE) 
+		{	
+			// (re)display form
+			$data = array(
+				'title' => 'Add Project',
+				'main' => 'projects/add'
+			);
+			
+			$this->load->view('template', $data);
+		}
+		else
+		{
+			$data['name'] = $this->input->post(name);
+			$this->project->insert($data);
 		}
 	}
+
+	/**
+	 * Displays the edit form and evaluates submits. If the submit validates properly, 
+	 * it makes change to project in database and redirects.
+	 *
+	 */
+	function edit() 
+	{
+		$id = (int) $this->uri->segment(3);
+
+		// display (or redisplay) the edit form
+		$data = array('project' => $this->project->read($id),
+		              'title'   => 'Edit Project',
+		              'main'    => 'projects/edit'
+		);
+		
+		$this->load->view('template', $data);
+	}
 	
-	function delete($id = null) {
-		if ($id == null) {
-			// error msg
-		} else {
-			// request delete from DB
+	/**
+	 * 
+	 *
+	 * @return void
+	 **/
+	function edit_action() 
+	{	
+		$data['id']   = $this->input->post('id');
+		$data['name'] = $this->input->post('name');
+		
+		if ($this->form_validation == FALSE)
+		{
+			$data['main'] = 'projects/edit'.$data['id'];
+			$this->load->view('template', $data);
+		}
+		else
+		{
+			// edit is valid, change the information
+			$this->project->update($data);
+			// $this->session->set_flashdata('message', '<div id="message">Edited successfully.</div>');
+			redirect('projects');
+		}
+		
+	}
+	
+	
+	// VALIDATION SECTION
+
+	/**
+	 * Callback: Returns true if the name exists
+	 *
+	 * @param string $name
+	 * @return boolean
+	 */
+	function name_exists($name) 
+	{
+		if ($this->project->count_named($name))
+		{
+			return TRUE;
+		}
+		else 
+		{
+			$this->validation->set_message('name_exists', 'The project %s aly exists.');
+			return FALSE;
 		}
 	}
 }
+
 
 /* End of file projects.php */
 /* Location: ./system/application/controllers/projects.php */
--- trunk/system/application/models/project.php
+++ trunk/system/application/models/project.php
@@ -4,63 +4,80 @@
  *
  * @package ChemDatabase
  **/ 
-class Project extends Model {
+class Project extends IgnitedRecord {
 	
 	/**
 	 * Construct the class and load the database.
 	 *
 	 * @return void
 	 **/
-	function Project() {
+	function Project() 
+	{
 		parent::Model();
-		$this->load->database('base_chem');
 	}
+	
 	
 	/**
 	 * Get all project records.
 	 *
 	 * @return object
 	 **/
-	function get_all() {
-		$q = $this->db->get('projects');
-		if ($q->num_rows() > 0) {
-			$data = $q->result();
-		}
-		
-		$q->free_result();
-		return $data;
+	function read_all() 
+	{
+		return $this->db->get('project');
 	}
 	
 	/**
 	 * Get an individual project.
 	 *
 	 * @param integer project's id number
-	 * @return void
+	 * @return array
 	 */
-	function get_project($id) {
-		$q = $this->db->get_where('projects', array('id' => $id), 1);
-		if ($q->num_rows() > 0) {
-			$data = $q->row();
-		}
-		
-		$q->free_result();
-		return $data;
+	function read($id) 
+	{
+		return $this->db->where('id', $id)->get('project')->row();
+	}
+	
+	/**
+	 * Set values for an individual project.
+	 *
+	 * @param integer project's id number
+	 * @param string project name
+	 * @return boolean
+	 */
+	function update($data) 
+	{
+		return $this->db->where('id', $data['id'])->update('project', $data);
 	}
 	
 	/**
 	 * Insert a project into the projects table.
 	 *
 	 * @param array Associative array of field => value.
-	 * @return void
+	 * @return int inserted id numer or -1 if insert failed
 	 **/
-	function insert_project($data) {
-		$q = $this->db->get_where('projects', array('name' => $data->name), 1);
-		if ($this->db->num_results() < 0) {
-			$this->db->insert('projects', $data);
-		} else {
-			// already exists
+	function create($data) 
+	{
+		if($this->db->insert('project', $data)) 
+		{
+			return $this->db->insert_id();
 		}
-		
+		else 
+		{
+			return -1;
+		}
+	}
+	
+	/**
+	 * Returns true if project is name is already present in the database.
+	 * Generally used from the callback.
+	 *
+	 * @param string Project name
+	 * @return boolean
+	 **/
+	function count_named($name) 
+	{
+		return $this->db->where('name', $name)->count_all_results('project');
 	}
 	
 }
