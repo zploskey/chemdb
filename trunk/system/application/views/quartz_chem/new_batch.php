45e6c92207337b7c05e1be724d403122af8393ee
Zach Ploskey <zploskey@gmail.com>
2009-01-31 08:24:31 +0000

    Some form validation message changes.  Relatively complete batches MVC and validation.  Changed batch desc field to description.  Minor changes to MY_Model: added get_where function. create_chemdb.sql in root directory has all the sql to take the currently running database, copy over the data, and modify it for the new schema.
    git-svn-id: svn+ssh://ovid.u.washington.edu/rc22/d09/cosmolab/repos/svn/chem_db@30 b159389c-fc5d-0410-935c-b53355241f03

---
+++ trunk/system/application/views/quartz_chem/new_batch.php
@@ -0,0 +1,65 @@
+<p><?=validation_errors()?></p>
+
+<table width="800" class="arial10">
+	
+	<?=form_open(site_url('quartz_chem/new_batch'))?>
+	
+	<input type=hidden name="id" value="<?=$batch->id?>">
+	<input type=hidden name="is_refresh" value="true">
+	
+		<tr>
+			<td width="400"> Batch ID </td>
+			<td><?=$batch->id?></td>
+		</tr>
+		<tr>
+			<td>
+				Batch start date:
+			</td>
+			<td><?=$batch->start_date?></td>
+		</tr>
+		<tr>
+			<td>
+				Number of samples:
+			</td>
+			<td>
+				<?php if ($allow_num_edit): ?>
+					<input type="text" name="numsamples" size=50 value="<?=$numsamples?>">
+				<?php else: ?>
+					<input type="hidden" name="numsamples" size=50 value="<?=$numsamples?>">
+					<?=$numsamples?>
+				<?php endif; ?>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Your initials:
+			</td>
+			<td>
+				<input type="text" name="owner" size=50 value="<?=$batch->owner?>">
+			</td>
+		</tr>
+		<tr>
+			<td>Short batch description:</td>
+			<td>
+				<textarea name="description" rows=2 cols=50><?=$batch->description?></textarea>
+			</td>
+		</tr>
+			<td colspan=2 align=center><hr>
+				</p><input type=submit value="Save and refresh">
+				</p><hr>
+			</td>
+		</tr>
+	</form>
+ 
+
+	<tr>
+		<?=form_open(site_url('quartz_chem/load_samples'))?>
+			<input type=hidden name="id" value="<?=$batch->id?>">
+			<td colspan=2 align=center>
+				<p/>
+				<input type=submit value="Onward to sample loading and carrier addition">
+				<hr>
+			</td>
+		</form>
+	</tr>
+</table>
