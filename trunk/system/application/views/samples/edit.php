940bfdf7922ea4f782e488430662ebeb5fdac184
cosmolab <cosmolab@b159389c-fc5d-0410-935c-b53355241f03>
2009-04-09 01:46:42 +0000

    Moved to using Doctrine for database access. Base model classes are now generated by running the db_rebuild script (requires bash) whenever there is a change made to the database. Sample loading still has some problems with working with samples that have just been added. Added two fields and changed some units for the sample database for editing but still need to change the viewing page.
    git-svn-id: svn+ssh://ovid.u.washington.edu/rc22/d09/cosmolab/repos/svn/chem_db@32 b159389c-fc5d-0410-935c-b53355241f03

--- trunk/system/application/views/samples/edit.php
+++ trunk/system/application/views/samples/edit.php
@@ -20,20 +20,26 @@
 		<input type="text" name="longitude" value="<?=$sample->longitude?>" size="10" /><br>
 		
 		<label>Altitude:</label>
-		<input type="text" name="altitude" value="<?=$sample->altitude?>" size="10" /> m<br>
+		<input type="text" name="altitude" value="<?=$sample->altitude?>" size="10" /> &nbsp;m<br>
 
 		<label>Shield factor:</label>
 		<input type="text" name="shield_factor" value="<?=$sample->shield_factor?>" size="10" /><br>
 
 		<label>Depth (top):</label>
-		<input type="text" name="depth_top" value="<?=$sample->depth_top?>" size="10" /> m<br>
+		<input type="text" name="depth_top" value="<?=$sample->depth_top?>" size="10" /> &nbsp;cm<br>
 
 		<label>Depth (bottom):</label>
-		<input type="text" name="depth_bottom" value="<?=$sample->depth_bottom?>" size="10" /> m<br>
+		<input type="text" name="depth_bottom" value="<?=$sample->depth_bottom?>" size="10" /> &nbsp;cm<br>
 
 		<label>Density:</label>
-		<input type="text" name="density" value="<?=$sample->density?>" size="10" /> kg/m^3<br>
+		<input type="text" name="density" value="<?=$sample->density?>" size="10" /> &nbsp;g/cm^3<br>
+
+		<label>Erosion Rate:</label>
+		<input type="text" name="erosion_rate" value="<?=$sample->erosion_rate?>" size="10" /> &nbsp;cm/y<br>
+
+		<label>Notes:</label>
+		<textarea name="notes" rows="5" cols="50" wrap="soft"><?=$sample->notes?></textarea><br>
 	</div>
 	
 	<p><input type="submit" value="Submit" /></p>
-</form>
+<?=form_close()?>
--- trunk/system/application/views/welcome.php
+++ trunk/system/application/views/welcome.php
@@ -1,3 +1,4 @@
 <p><?=anchor('projects', 'Manage Projects')?></p>
 <p><?=anchor('samples', 'Manage Samples')?></p>
 <p><?=anchor('quartz_chem','Quartz Chemistry')?></p>
+<p><?=anchor('doct', 'doct')?></p>
