da9bd2e3e29e1299ccd10d1d58dc76733133aff7
Zach Ploskey <zploskey@gmail.com>
2009-04-28 20:38:58 +0000

    Updated samples pages to use Doctrine's merge function, makes code much more readable and shorter. This also fixed a strange bug where the new notes and erosion rate fields weren't refreshing with postdata after an error.
    git-svn-id: svn+ssh://ovid.u.washington.edu/rc22/d09/cosmolab/repos/svn/chem_db@46 b159389c-fc5d-0410-935c-b53355241f03

--- trunk/system/application/views/samples/edit.php
+++ trunk/system/application/views/samples/edit.php
@@ -7,38 +7,39 @@
 <p><h2><?=$subtitle?></h2></p>
 
 <?=form_open(site_url("samples/edit/$arg"))?>
+	<input type="hidden" name="is_refresh" value="TRUE">
 	<p><?=validation_errors()?></p><p/>
 
 	<div class="formblock">
 		<label>Name:</label>
-		<?=form_input('name', $sample->name)?><br>
+		<?=form_input('sample[name]', $sample->name)?><br>
 				
 		<label>Latitude:</label>
-		<input type="text" name="latitude" value="<?=$sample->latitude?>" size="10" /> <br>
+		<input type="text" name="sample[latitude]" value="<?=$sample->latitude?>" size="10" /> &nbsp;Northern hemisphere = positive<br>
 
 		<label>Longitude:</label>
-		<input type="text" name="longitude" value="<?=$sample->longitude?>" size="10" /> &nbsp;Northern hemisphere = positive<br>
+		<input type="text" name="sample[longitude]" value="<?=$sample->longitude?>" size="10" /> &nbsp;East is positive, West negative<br>
 		
 		<label>Altitude:</label>
-		<input type="text" name="altitude" value="<?=$sample->altitude?>" size="10" /> &nbsp;m<br>
+		<input type="text" name="sample[altitude]" value="<?=$sample->altitude?>" size="10" /> &nbsp;m<br>
 
 		<label>Shield factor:</label>
-		<input type="text" name="shield_factor" value="<?=$sample->shield_factor?>" size="10" /><br>
+		<input type="text" name="sample[shield_factor]" value="<?=$sample->shield_factor?>" size="10" /><br>
 
 		<label>Depth (top):</label>
-		<input type="text" name="depth_top" value="<?=$sample->depth_top?>" size="10" /> &nbsp;cm<br>
+		<input type="text" name="sample[depth_top]" value="<?=$sample->depth_top?>" size="10" /> &nbsp;cm<br>
 
 		<label>Depth (bottom):</label>
-		<input type="text" name="depth_bottom" value="<?=$sample->depth_bottom?>" size="10" /> &nbsp;cm<br>
+		<input type="text" name="sample[depth_bottom]" value="<?=$sample->depth_bottom?>" size="10" /> &nbsp;cm<br>
 
 		<label>Density:</label>
-		<input type="text" name="density" value="<?=$sample->density?>" size="10" /> &nbsp;g/cm^3<br>
+		<input type="text" name="sample[density]" value="<?=$sample->density?>" size="10" /> &nbsp;g/cm^3<br>
 
 		<label>Erosion Rate:</label>
-		<input type="text" name="erosion_rate" value="<?=$sample->erosion_rate?>" size="10" /> &nbsp;cm/y<br>
+		<input type="text" name="sample[erosion_rate]" value="<?=$sample->erosion_rate?>" size="10" /> &nbsp;cm/y<br>
 
 		<label>Notes:</label>
-		<textarea name="notes" rows="5" cols="50" wrap="soft"><?=$sample->notes?></textarea><br>
+		<textarea name="sample[notes]" rows="5" cols="50" wrap="soft"><?=$sample->notes?></textarea><br>
 	</div>
 	
 	<p><input type="submit" value="Submit" /></p>
