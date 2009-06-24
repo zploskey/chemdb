be98dd84bae13a72960d8e1ee975d18bc4cd4429
Zach Ploskey <zploskey@gmail.com>
2009-06-24 23:57:37 +0000

    Starting work on AlChecks. Index is working as desired, alchecks/sample_loading is a stub.
    git-svn-id: svn+ssh://ovid.u.washington.edu/rc22/d09/cosmolab/repos/svn/chem_db@65 b159389c-fc5d-0410-935c-b53355241f03

---
+++ trunk/system/application/models/AlcheckBatchTable.php
@@ -0,0 +1,31 @@
+<?php 
+
+class AlcheckBatchTable extends Doctrine_Table
+{
+    /**
+	 *
+	 * @return Doctrine_Collection
+	 */
+	public function findRecentBatches($nBatches)
+	{
+		return Doctrine_Query::create()
+			->from('AlcheckBatch b')
+			->orderBy('b.prep_date desc')
+			->where('b.description != ?', 'Dummy batch')
+			->limit($nBatches)
+			->execute();
+	}
+	
+    /**
+	 *
+	 * @return Doctrine_Collection
+	 */
+	public function findAllBatches()
+	{
+		return Doctrine_Query::create()
+			->from('AlcheckBatch b')
+			->orderBy('b.prep_date desc')
+			->where('b.description != ?', 'Dummy batch')
+			->execute();
+	}
+}
