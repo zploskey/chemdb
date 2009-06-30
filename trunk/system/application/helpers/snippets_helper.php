1cc60b5e6dc891e213a0c34708390d80c662cafc
cosmolab <cosmolab@b159389c-fc5d-0410-935c-b53355241f03>
2009-06-30 22:34:45 +0000

    quartz_chem/add_icp_results works like a charm, error-checking included.
    git-svn-id: svn+ssh://ovid.u.washington.edu/rc22/d09/cosmolab/repos/svn/chem_db@68 b159389c-fc5d-0410-935c-b53355241f03

--- trunk/system/application/helpers/snippets_helper.php
+++ trunk/system/application/helpers/snippets_helper.php
@@ -13,20 +13,48 @@ function switch_sort($cur, $default = 'asc')
 	$cur = strtolower($cur);
 	$default = strtolower($default);
 	
-	if ($default == 'desc')
-	{
+	if ($default == 'desc') {
 		$alt = 'asc';
-	}
-	else
-	{
+	} else {
 		$alt = 'desc';
 	}
 	
-	if ($cur == $default)
-	{
+	if ($cur == $default) {
 		return $alt;
 	}
 	
 	return $default;
 }
 
+/**
+ * Takes an array and returns a string of the elements listed as in a sentence.
+ * 
+ * Example: 
+ * $arr = array('blue', 'red, 'green', 'yellow');
+ * echo comma_list($arr)
+ * 
+ * Yields: "blue, red, green and yellow"
+ *
+ * @param array $arr array of elements with string representations
+ * @return string $str comma separated list, including an and before the last element
+ * @author cosmolab
+ **/
+function comma_str($arr, $do_and = false)
+{
+    $len = count($arr);
+    $str = '';
+    for($i = 0; $i < $len; $i++) {
+        // don't print a comma before the first item
+        if ($i != 0) {
+            // put and before the last item
+            if ($do_and AND $i == $len - 1) {
+                $str .= ' and ';
+            } else {
+                // everywhere else we print a comma before the item
+                $str .= ', ';
+            }
+        }
+        $str .= $arr[$i];
+    }
+    return $str;
+}
