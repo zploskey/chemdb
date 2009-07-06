b75199f101d35ccbe5e9e4bdfabc76cc28f228a2
Zach Ploskey <zploskey@gmail.com>
2009-07-06 23:22:40 +0000

    Doctrine 1.1.2-generated models.
    git-svn-id: svn+ssh://ovid.u.washington.edu/rc22/d09/cosmolab/repos/svn/chem_db@75 b159389c-fc5d-0410-935c-b53355241f03

--- trunk/system/application/models/generated/BaseAnalysis.php
+++ trunk/system/application/models/generated/BaseAnalysis.php
@@ -10,6 +10,7 @@
  * @property integer $number_within_batch
  * @property string $sample_name
  * @property string $sample_type
+ * @property integer $diss_bottle_id
  * @property string $diss_bottle_number
  * @property float $wt_diss_bottle_tare
  * @property float $wt_diss_bottle_sample
@@ -18,54 +19,166 @@
  * @property float $wt_diss_bottle_total
  * @property string $notes
  * @property integer $sample_id
- * @property integer $diss_bottle_id
  * @property Batch $Batch
- * @property Sample $Sample
  * @property DissBottle $DissBottle
+ * @property Sample $Sample
  * @property Doctrine_Collection $AlcheckAnalysis
  * @property Doctrine_Collection $Split
  * 
  * @package    ##PACKAGE##
  * @subpackage ##SUBPACKAGE##
  * @author     ##NAME## <##EMAIL##>
- * @version    SVN: $Id: Builder.php 5441 2009-01-30 22:58:43Z jwage $
+ * @version    SVN: $Id: Builder.php 5845 2009-06-09 07:36:57Z jwage $
  */
 abstract class BaseAnalysis extends Doctrine_Record
 {
     public function setTableDefinition()
     {
         $this->setTableName('analysis');
-        $this->hasColumn('id', 'integer', 2, array('type' => 'integer', 'unsigned' => '1', 'primary' => true, 'autoincrement' => true, 'length' => '2'));
-        $this->hasColumn('batch_id', 'integer', 2, array('type' => 'integer', 'default' => '0', 'notnull' => true, 'length' => '2'));
-        $this->hasColumn('number_within_batch', 'integer', 1, array('type' => 'integer', 'default' => '0', 'notnull' => true, 'length' => '1'));
-        $this->hasColumn('sample_name', 'string', 2147483647, array('type' => 'string', 'default' => '', 'notnull' => true, 'length' => '2147483647'));
-        $this->hasColumn('sample_type', 'string', 2147483647, array('type' => 'string', 'default' => '', 'notnull' => true, 'length' => '2147483647'));
-        $this->hasColumn('diss_bottle_number', 'string', 2147483647, array('type' => 'string', 'default' => '', 'notnull' => true, 'length' => '2147483647'));
-        $this->hasColumn('wt_diss_bottle_tare', 'float', 2147483647, array('type' => 'float', 'default' => '0', 'notnull' => true, 'length' => '2147483647'));
-        $this->hasColumn('wt_diss_bottle_sample', 'float', 2147483647, array('type' => 'float', 'default' => '0', 'notnull' => true, 'length' => '2147483647'));
-        $this->hasColumn('wt_be_carrier', 'float', 2147483647, array('type' => 'float', 'default' => '0', 'notnull' => true, 'length' => '2147483647'));
-        $this->hasColumn('wt_al_carrier', 'float', 2147483647, array('type' => 'float', 'default' => '0', 'notnull' => true, 'length' => '2147483647'));
-        $this->hasColumn('wt_diss_bottle_total', 'float', 2147483647, array('type' => 'float', 'default' => '0', 'notnull' => true, 'length' => '2147483647'));
-        $this->hasColumn('notes', 'string', 2147483647, array('type' => 'string', 'default' => '', 'notnull' => true, 'length' => '2147483647'));
-        $this->hasColumn('sample_id', 'integer', 4, array('type' => 'integer', 'unsigned' => '1', 'length' => '4'));
-        $this->hasColumn('diss_bottle_id', 'integer', 4, array('type' => 'integer', 'length' => '4'));
+        $this->hasColumn('id', 'integer', 2, array(
+             'type' => 'integer',
+             'unsigned' => '1',
+             'primary' => true,
+             'autoincrement' => true,
+             'length' => '2',
+             ));
+        $this->hasColumn('batch_id', 'integer', 2, array(
+             'type' => 'integer',
+             'unsigned' => 0,
+             'primary' => false,
+             'default' => '0',
+             'notnull' => true,
+             'autoincrement' => false,
+             'length' => '2',
+             ));
+        $this->hasColumn('number_within_batch', 'integer', 1, array(
+             'type' => 'integer',
+             'unsigned' => 0,
+             'primary' => false,
+             'default' => '0',
+             'notnull' => true,
+             'autoincrement' => false,
+             'length' => '1',
+             ));
+        $this->hasColumn('sample_name', 'string', 2147483647, array(
+             'type' => 'string',
+             'fixed' => 0,
+             'primary' => false,
+             'default' => '',
+             'notnull' => true,
+             'autoincrement' => false,
+             'length' => '2147483647',
+             ));
+        $this->hasColumn('sample_type', 'string', 2147483647, array(
+             'type' => 'string',
+             'fixed' => 0,
+             'primary' => false,
+             'default' => '',
+             'notnull' => true,
+             'autoincrement' => false,
+             'length' => '2147483647',
+             ));
+        $this->hasColumn('diss_bottle_id', 'integer', 4, array(
+             'type' => 'integer',
+             'unsigned' => 0,
+             'primary' => false,
+             'notnull' => false,
+             'autoincrement' => false,
+             'length' => '4',
+             ));
+        $this->hasColumn('diss_bottle_number', 'string', 2147483647, array(
+             'type' => 'string',
+             'fixed' => 0,
+             'primary' => false,
+             'default' => '',
+             'notnull' => true,
+             'autoincrement' => false,
+             'length' => '2147483647',
+             ));
+        $this->hasColumn('wt_diss_bottle_tare', 'float', 2147483647, array(
+             'type' => 'float',
+             'unsigned' => 0,
+             'primary' => false,
+             'default' => '0',
+             'notnull' => true,
+             'autoincrement' => false,
+             'length' => '2147483647',
+             ));
+        $this->hasColumn('wt_diss_bottle_sample', 'float', 2147483647, array(
+             'type' => 'float',
+             'unsigned' => 0,
+             'primary' => false,
+             'default' => '0',
+             'notnull' => true,
+             'autoincrement' => false,
+             'length' => '2147483647',
+             ));
+        $this->hasColumn('wt_be_carrier', 'float', 2147483647, array(
+             'type' => 'float',
+             'unsigned' => 0,
+             'primary' => false,
+             'default' => '0',
+             'notnull' => true,
+             'autoincrement' => false,
+             'length' => '2147483647',
+             ));
+        $this->hasColumn('wt_al_carrier', 'float', 2147483647, array(
+             'type' => 'float',
+             'unsigned' => 0,
+             'primary' => false,
+             'default' => '0',
+             'notnull' => true,
+             'autoincrement' => false,
+             'length' => '2147483647',
+             ));
+        $this->hasColumn('wt_diss_bottle_total', 'float', 2147483647, array(
+             'type' => 'float',
+             'unsigned' => 0,
+             'primary' => false,
+             'default' => '0',
+             'notnull' => true,
+             'autoincrement' => false,
+             'length' => '2147483647',
+             ));
+        $this->hasColumn('notes', 'string', 2147483647, array(
+             'type' => 'string',
+             'fixed' => 0,
+             'primary' => false,
+             'default' => '',
+             'notnull' => true,
+             'autoincrement' => false,
+             'length' => '2147483647',
+             ));
+        $this->hasColumn('sample_id', 'integer', 4, array(
+             'type' => 'integer',
+             'unsigned' => '1',
+             'primary' => false,
+             'notnull' => false,
+             'autoincrement' => false,
+             'length' => '4',
+             ));
     }
 
     public function setUp()
     {
-        $this->hasOne('Batch', array('local' => 'batch_id',
-                                     'foreign' => 'id'));
+        $this->hasOne('Batch', array(
+             'local' => 'batch_id',
+             'foreign' => 'id'));
 
-        $this->hasOne('Sample', array('local' => 'sample_id',
-                                      'foreign' => 'id'));
+        $this->hasOne('DissBottle', array(
+             'local' => 'diss_bottle_id',
+             'foreign' => 'id'));
 
-        $this->hasOne('DissBottle', array('local' => 'diss_bottle_id',
-                                          'foreign' => 'id'));
+        $this->hasOne('Sample', array(
+             'local' => 'sample_id',
+             'foreign' => 'id'));
 
-        $this->hasMany('AlcheckAnalysis', array('local' => 'id',
-                                                'foreign' => 'analysis_id'));
+        $this->hasMany('AlcheckAnalysis', array(
+             'local' => 'id',
+             'foreign' => 'analysis_id'));
 
-        $this->hasMany('Split', array('local' => 'id',
-                                      'foreign' => 'analysis_id'));
+        $this->hasMany('Split', array(
+             'local' => 'id',
+             'foreign' => 'analysis_id'));
     }
 }
--- trunk/system/application/models/generated/BaseSplit.php
+++ trunk/system/application/models/generated/BaseSplit.php
@@ -9,10 +9,10 @@
  * @property integer $analysis_id
  * @property integer $split_bkr_id
  * @property integer $split_num
+ * @property string $split_bkr_name
  * @property float $wt_split_bkr_tare
  * @property float $wt_split_bkr_split
  * @property float $wt_split_bkr_icp
- * @property string $split_bkr_name
  * @property Analysis $Analysis
  * @property SplitBkr $SplitBkr
  * @property Doctrine_Collection $IcpRun
@@ -20,32 +20,96 @@
  * @package    ##PACKAGE##
  * @subpackage ##SUBPACKAGE##
  * @author     ##NAME## <##EMAIL##>
- * @version    SVN: $Id: Builder.php 5441 2009-01-30 22:58:43Z jwage $
+ * @version    SVN: $Id: Builder.php 5845 2009-06-09 07:36:57Z jwage $
  */
 abstract class BaseSplit extends Doctrine_Record
 {
     public function setTableDefinition()
     {
         $this->setTableName('split');
-        $this->hasColumn('id', 'integer', 4, array('type' => 'integer', 'unsigned' => '1', 'primary' => true, 'autoincrement' => true, 'length' => '4'));
-        $this->hasColumn('analysis_id', 'integer', 2, array('type' => 'integer', 'unsigned' => '1', 'default' => '', 'notnull' => true, 'length' => '2'));
-        $this->hasColumn('split_bkr_id', 'integer', 4, array('type' => 'integer', 'unsigned' => '1', 'default' => '', 'notnull' => true, 'length' => '4'));
-        $this->hasColumn('split_num', 'integer', 1, array('type' => 'integer', 'unsigned' => '1', 'default' => '1', 'notnull' => true, 'length' => '1'));
-        $this->hasColumn('wt_split_bkr_tare', 'float', 2147483647, array('type' => 'float', 'default' => '0', 'notnull' => true, 'length' => '2147483647'));
-        $this->hasColumn('wt_split_bkr_split', 'float', 2147483647, array('type' => 'float', 'default' => '0', 'notnull' => true, 'length' => '2147483647'));
-        $this->hasColumn('wt_split_bkr_icp', 'float', 2147483647, array('type' => 'float', 'default' => '0', 'notnull' => true, 'length' => '2147483647'));
-        $this->hasColumn('split_bkr_name', 'string', 2147483647, array('type' => 'string', 'length' => '2147483647'));
+        $this->hasColumn('id', 'integer', 4, array(
+             'type' => 'integer',
+             'unsigned' => '1',
+             'primary' => true,
+             'autoincrement' => true,
+             'length' => '4',
+             ));
+        $this->hasColumn('analysis_id', 'integer', 2, array(
+             'type' => 'integer',
+             'unsigned' => '1',
+             'primary' => false,
+             'default' => '',
+             'notnull' => true,
+             'autoincrement' => false,
+             'length' => '2',
+             ));
+        $this->hasColumn('split_bkr_id', 'integer', 4, array(
+             'type' => 'integer',
+             'unsigned' => '1',
+             'primary' => false,
+             'default' => '',
+             'notnull' => true,
+             'autoincrement' => false,
+             'length' => '4',
+             ));
+        $this->hasColumn('split_num', 'integer', 1, array(
+             'type' => 'integer',
+             'unsigned' => '1',
+             'primary' => false,
+             'default' => '1',
+             'notnull' => true,
+             'autoincrement' => false,
+             'length' => '1',
+             ));
+        $this->hasColumn('split_bkr_name', 'string', 2147483647, array(
+             'type' => 'string',
+             'fixed' => 0,
+             'primary' => false,
+             'notnull' => false,
+             'autoincrement' => false,
+             'length' => '2147483647',
+             ));
+        $this->hasColumn('wt_split_bkr_tare', 'float', 2147483647, array(
+             'type' => 'float',
+             'unsigned' => 0,
+             'primary' => false,
+             'default' => '0',
+             'notnull' => true,
+             'autoincrement' => false,
+             'length' => '2147483647',
+             ));
+        $this->hasColumn('wt_split_bkr_split', 'float', 2147483647, array(
+             'type' => 'float',
+             'unsigned' => 0,
+             'primary' => false,
+             'default' => '0',
+             'notnull' => true,
+             'autoincrement' => false,
+             'length' => '2147483647',
+             ));
+        $this->hasColumn('wt_split_bkr_icp', 'float', 2147483647, array(
+             'type' => 'float',
+             'unsigned' => 0,
+             'primary' => false,
+             'default' => '0',
+             'notnull' => true,
+             'autoincrement' => false,
+             'length' => '2147483647',
+             ));
     }
 
     public function setUp()
     {
-        $this->hasOne('Analysis', array('local' => 'analysis_id',
-                                        'foreign' => 'id'));
+        $this->hasOne('Analysis', array(
+             'local' => 'analysis_id',
+             'foreign' => 'id'));
 
-        $this->hasOne('SplitBkr', array('local' => 'split_bkr_id',
-                                        'foreign' => 'id'));
+        $this->hasOne('SplitBkr', array(
+             'local' => 'split_bkr_id',
+             'foreign' => 'id'));
 
-        $this->hasMany('IcpRun', array('local' => 'id',
-                                       'foreign' => 'split_id'));
+        $this->hasMany('IcpRun', array(
+             'local' => 'id',
+             'foreign' => 'split_id'));
     }
 }
