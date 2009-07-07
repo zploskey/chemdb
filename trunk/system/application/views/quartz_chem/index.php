d9ed69785d3a82590b0afaa8d9539699ad4f2781
Zach Ploskey <zploskey@gmail.com>
2009-07-07 04:03:40 +0000

    Formatting, some minor oversights fixed in helper functions, converting tabs to spaces in some overlooked files. Fixed alchecks/index page load errors.
    git-svn-id: svn+ssh://ovid.u.washington.edu/rc22/d09/cosmolab/repos/svn/chem_db@77 b159389c-fc5d-0410-935c-b53355241f03

--- trunk/system/application/views/quartz_chem/index.php
+++ trunk/system/application/views/quartz_chem/index.php
@@ -1,201 +1,201 @@
 <table>
-	<!--- CREATE A NEW BATCH -->
-	<!-- action=start_new_quartz_prep.php -->
-	<?=form_open('quartz_chem/new_batch')?>
-		<tr>
-			<td><b><i>Create a new batch</i></b></td>
-			<td align=center>
-				<input type=submit width=40 value="Create a new batch">
-			</td>
-		</tr>
-	<?=form_close()?>
+    <!--- CREATE A NEW BATCH -->
+    <!-- action=start_new_quartz_prep.php -->
+    <?=form_open('quartz_chem/new_batch')?>
+        <tr>
+            <td><b><i>Create a new batch</i></b></td>
+            <td align=center>
+                <input type=submit width=40 value="Create a new batch">
+            </td>
+        </tr>
+    <?=form_close()?>
 
-	<tr><td colspan=2><hr></td></tr>
-	<p/>
-	<!--- SAMPLE LOADING AND CARRIER ADDITION -->
+    <tr><td colspan=2><hr></td></tr>
+    <p/>
+    <!--- SAMPLE LOADING AND CARRIER ADDITION -->
 
-	<tr>
-		<td colspan=2><i><b>Sample loading and carrier addition</b></i></td>
-	</tr>
+    <tr>
+        <td colspan=2><i><b>Sample loading and carrier addition</b></i></td>
+    </tr>
 
-	<!-- action=quartz_sample_loading.php -->
-	<?=form_open(site_url('quartz_chem/load_samples'))?>
-		<tr>
-			<td align=center>
-				<i>Batch:</i>
-				<select name="batch_id">
-					<?=$open_batches?>
-				</select>
+    <!-- action=quartz_sample_loading.php -->
+    <?=form_open(site_url('quartz_chem/load_samples'))?>
+        <tr>
+            <td align=center>
+                <i>Batch:</i>
+                <select name="batch_id">
+                    <?=$open_batches?>
+                </select>
 
-			</td>
-			<td align=center>
-				<input type="submit" value="Sample loading and carrier addition for this batch">
-			</td>
-		</tr>
-	<?=form_close()?>
+            </td>
+            <td align=center>
+                <input type="submit" value="Sample loading and carrier addition for this batch">
+            </td>
+        </tr>
+    <?=form_close()?>
 
-	<tr><td colspan=2><hr/></td></tr>
-	<p/>
+    <tr><td colspan=2><hr/></td></tr>
+    <p/>
 
-	<!--- PRINT THE TRACKING SHEET -->
+    <!--- PRINT THE TRACKING SHEET -->
 
-	<tr>
-		<td colspan=2><i><b>Print lab tracking sheet</b></i></td>
-	</tr>
-	<!-- action=print_tracking_sheet.php -->
-	<?=form_open('quartz_chem/print_tracking_sheet')?>
-		<tr>
-			<td align=center>
-				<i>Batch:</i>
-				<select name="batch_id">
-					<?=$open_batches?>
-				</select>
-			</td>
-			<td align=center>
-				<input type=submit value="Print tracking sheet for this batch">
-			</td>
-		</tr>
-	<?=form_close()?>
-	<tr><td colspan=2><hr></td></tr>
-	<p/>
+    <tr>
+        <td colspan=2><i><b>Print lab tracking sheet</b></i></td>
+    </tr>
+    <!-- action=print_tracking_sheet.php -->
+    <?=form_open('quartz_chem/print_tracking_sheet')?>
+        <tr>
+            <td align=center>
+                <i>Batch:</i>
+                <select name="batch_id">
+                    <?=$open_batches?>
+                </select>
+            </td>
+            <td align=center>
+                <input type=submit value="Print tracking sheet for this batch">
+            </td>
+        </tr>
+    <?=form_close()?>
+    <tr><td colspan=2><hr></td></tr>
+    <p/>
 
-	<!--- ADD TOTAL SOLUTION WEIGHTS -->
+    <!--- ADD TOTAL SOLUTION WEIGHTS -->
 
-	<tr>
-		<td colspan=2><i><b>Add total solution weights</b></i></td>
-	</tr>
-	<!-- action=add_total_weights.php -->
-	<?=form_open('quartz_chem/add_solution_weights')?>
-		<tr>
-			<td align=center>
-				<i>Batch:</i>
-				<select name="batch_id">
-					<?=$open_batches?>
-				</select>
-			</td>
-			<td align=center>
-				<input type=submit value="Add total solution weights for this batch">
-			</td>
-		</tr>
-	<?=form_close()?>
-	<tr><td colspan=2><hr></td></tr>
-	<p/>
+    <tr>
+        <td colspan=2><i><b>Add total solution weights</b></i></td>
+    </tr>
+    <!-- action=add_total_weights.php -->
+    <?=form_open('quartz_chem/add_solution_weights')?>
+        <tr>
+            <td align=center>
+                <i>Batch:</i>
+                <select name="batch_id">
+                    <?=$open_batches?>
+                </select>
+            </td>
+            <td align=center>
+                <input type=submit value="Add total solution weights for this batch">
+            </td>
+        </tr>
+    <?=form_close()?>
+    <tr><td colspan=2><hr></td></tr>
+    <p/>
 
-	<!--- ADD SPLIT WEIGHTS -->
+    <!--- ADD SPLIT WEIGHTS -->
 
-	<tr>
-		<td colspan=2>
-			<b><i>Add split weights</i></b>
-		</td>
-	</tr>
-	<!-- action=add_split_weights.php -->
-	<?=form_open('quartz_chem/add_split_weights')?>
-		<tr>
-			<td align=center>
-				<i>Batch:</i>   
-				<select name="batch_id">
-					<?=$open_batches?>
-				</select>
-			</td>
-			<td align=center>
-				<input type=submit value="Add split weights for this batch">
-			</td>
-		</tr>
-	<?=form_close()?>
-	<tr><td colspan=2><hr></td></tr><p/>
+    <tr>
+        <td colspan=2>
+            <b><i>Add split weights</i></b>
+        </td>
+    </tr>
+    <!-- action=add_split_weights.php -->
+    <?=form_open('quartz_chem/add_split_weights')?>
+        <tr>
+            <td align=center>
+                <i>Batch:</i>   
+                <select name="batch_id">
+                    <?=$open_batches?>
+                </select>
+            </td>
+            <td align=center>
+                <input type=submit value="Add split weights for this batch">
+            </td>
+        </tr>
+    <?=form_close()?>
+    <tr><td colspan=2><hr></td></tr><p/>
 
 
-	<!--- ADD ICP WEIGHTS -->
+    <!--- ADD ICP WEIGHTS -->
 
-	<tr>
-		<td colspan=2>
-			<i><b>Add ICP solution weights</b></i>
-		</td>
-	</tr>
-	
-	<?=form_open('quartz_chem/add_icp_weights')?>
-		<tr>
-			<td align=center>
-				<i>Batch:</i>   
-				<select name="batch_id">
-					<?=$open_batches?>
-				</select>
-			</td>
-			<td align=center>
-				<input type=submit value="Add ICP solution weights for this batch">
-			</td>
-		</tr>
-	<?=form_close()?>
+    <tr>
+        <td colspan=2>
+            <i><b>Add ICP solution weights</b></i>
+        </td>
+    </tr>
+    
+    <?=form_open('quartz_chem/add_icp_weights')?>
+        <tr>
+            <td align=center>
+                <i>Batch:</i>   
+                <select name="batch_id">
+                    <?=$open_batches?>
+                </select>
+            </td>
+            <td align=center>
+                <input type=submit value="Add ICP solution weights for this batch">
+            </td>
+        </tr>
+    <?=form_close()?>
 
-	<tr><td colspan=2><hr></td></tr><p/>
+    <tr><td colspan=2><hr></td></tr><p/>
 
-	<!--- ADD ICP RESULTS -->
+    <!--- ADD ICP RESULTS -->
 
-	<tr>
-		<td colspan=2>
-			<i><b>Enter ICP results</b></i>
-		</td>
-	</tr>
-	
-	<?=form_open('quartz_chem/add_icp_results')?>	
-		<tr>
-			<td align=center>
-				<i>Batch:</i>   
-				<select name="batch_id">
-					<?=$open_batches?>
-				</select>
-			</td>
-			<td align=center>
-				<input type=submit value="Add ICP results for this batch">
-			</td>
-		</tr>
-	<?=form_close()?>
-	<tr><td colspan=2><hr></p></td></tr>
+    <tr>
+        <td colspan=2>
+            <i><b>Enter ICP results</b></i>
+        </td>
+    </tr>
+    
+    <?=form_open('quartz_chem/add_icp_results')?>   
+        <tr>
+            <td align=center>
+                <i>Batch:</i>   
+                <select name="batch_id">
+                    <?=$open_batches?>
+                </select>
+            </td>
+            <td align=center>
+                <input type=submit value="Add ICP results for this batch">
+            </td>
+        </tr>
+    <?=form_close()?>
+    <tr><td colspan=2><hr></p></td></tr>
 
-	<!--- FINAL REPORT -->
+    <!--- FINAL REPORT -->
 
-	<tr>
-		<td colspan=2>
-			<i><b>Final report</b></i>
-		</td>
-	</tr>
+    <tr>
+        <td colspan=2>
+            <i><b>Final report</b></i>
+        </td>
+    </tr>
 
-	<?=form_open('quartz_chem/final_report')?>
-		<tr>
-			<td align=center>
-				<i>Batch:</i>
-				<select name="batch_id">
-					<?=$all_batches?>
-				</select>
+    <?=form_open('quartz_chem/final_report')?>
+        <tr>
+            <td align=center>
+                <i>Batch:</i>
+                <select name="batch_id">
+                    <?=$all_batches?>
+                </select>
 
-			</td>
-			<td align=center>
-				<input type=submit value="Get final report for this batch">
-			</td>
-		</tr>
-	<?=form_close()?>
-	<tr><td colspan=2><hr></td></tr>
+            </td>
+            <td align=center>
+                <input type=submit value="Get final report for this batch">
+            </td>
+        </tr>
+    <?=form_close()?>
+    <tr><td colspan=2><hr></td></tr>
 
-	<tr>
-		<td colspan=2>
-			<i><b>Take a completed batch off the active list</b></i>
-		</td>
-	</tr>
-	<!-- action="quartz_chem_options.php" -->
-	<?=form_open('quartz_chem/index')?>
-		<input type="hidden" name="is_lock" value="yes">
-		<tr>
-			<td align=center>
-				<i>Batch:</i>   
-				<select name="batch_id">
-					<?=$open_batches?>
-				</select>
-			</td>
-			<td align=center>
-				<input type=submit value="This batch is done!">
-			</td>
-		</tr>
-	<?=form_close()?>
+    <tr>
+        <td colspan=2>
+            <i><b>Take a completed batch off the active list</b></i>
+        </td>
+    </tr>
+    <!-- action="quartz_chem_options.php" -->
+    <?=form_open('quartz_chem/index')?>
+        <input type="hidden" name="is_lock" value="yes">
+        <tr>
+            <td align=center>
+                <i>Batch:</i>   
+                <select name="batch_id">
+                    <?=$open_batches?>
+                </select>
+            </td>
+            <td align=center>
+                <input type=submit value="This batch is done!">
+            </td>
+        </tr>
+    <?=form_close()?>
 
-	<tr><td colspan=2 ><hr></td></tr>
+    <tr><td colspan=2 ><hr></td></tr>
 </table>
