6b0c51456b45ca98469a84b4c427560b127453a5
cosmolab <cosmolab@b159389c-fc5d-0410-935c-b53355241f03>
2009-07-07 23:54:40 +0000

    ICP Quality Control page is now fully functional.
    git-svn-id: svn+ssh://ovid.u.washington.edu/rc22/d09/cosmolab/repos/svn/chem_db@79 b159389c-fc5d-0410-935c-b53355241f03

--- trunk/system/application/views/quartz_chem/icp_quality_control.php
+++ trunk/system/application/views/quartz_chem/icp_quality_control.php
@@ -19,8 +19,12 @@
     <tr><td><hr></td></tr>
 </table>
 
-<table width=800 class=arial8>
+<?php if ($errors) echo validation_errors(); ?>
 
+<table width=800 class=arial8>
+<?php 
+// save the column labels for repeated printing within the analysis loop
+echo $cols = <<<COL
 <tr><td colspan=11><hr></td></tr>
 <tr>
     <td>Analysis ID</td>
@@ -35,114 +39,94 @@
     <td>ug Al</td>
     <td>OK?</td>
 </tr>
+COL;
 
-<? for ($a = 0, $an = &$batch->Analysis; $a < $numsamples; $a++): ?>
+for ($a = 0; $a < $nsamples; $a++):
+    $an = &$batch->Analysis[$a]; ?>
+    <tr><td colspan="11"><hr></td></tr>
+    <tr>    
+    <? for ($s = 0; $s < $nsplits[$a]; $s++): ?>
     
-    <tr><td colspan=11><hr></td></tr>
-    <tr>
-        
-        <? for ($s = 0, $sp = &$an->Split, $nsp = $sp->count(); $s < $nsp; $s++): ?>
-        
-            <? if ($s == 0): ?>
-                <td><?=$an[$a]->id?></td>
-                <td><?=$an[$a]->sample_name?></td>
-            <? else: ?>
-                <td colspan="2"></td>
-            <? endif; ?>
-            
-        <!-- // Formerly column 5. Dil factor, split 1
-        unset($this_split_1_wt,$this_ICP_1_wt,$this_df_1); -->
-        $this_split_1_wt = $this_sample['wt_split_bkr_1_split'] - $this_sample['wt_split_bkr_1_tare'];
-        $this_ICP_1_wt = $this_sample['wt_split_bkr_1_ICP'] - $this_sample['wt_split_bkr_1_tare'];
-        $this_df_1 = $this_ICP_1_wt / $this_split_1_wt;
-        
-     <!--<td> <? // printf('%.3f',$this_df_1) ?> </td> -->
-        
-        <!-- Column 5. Run number -->
-            <? for ($r = 0, $run = &$sp->IcpRun, $nruns = $run->count(); $r < $nruns; $r++): ?>
-                
-                <? if ($r == 0):?>
-                    <td>Split <?=$sp->split_num?></td>
-                    <td><?=$sp->sample_name?></td>
-                <? else: ?>
-                    <td colspan="2"></td>
-                <? endif; ?>
-                
-                <td>Run <?=$r->run_num?></td>
-                <td><? printf('%.4f', $r->be_result); ?></td>
-                
-            $this_tot_Be_b1_r1 = $this_df_1 * $this_sample['ICP_Be_split1_run1'] * $this_soln_wt;
-                
-                <td><? printf('%.1f', $tot_be[$a][$s][$r]); ?></td>
-                <td>
-                    <?php 
-                    echo '<input type="checkbox" value="y" name="use_be[]" ';
-                    if ($use_be[$a][$s][$r] == 'y') {
-                        print "checked"; 
-                    }
-                    echo '>'; ?>
-                </td>
-                <td><? printf('%.4f', $r->al_result); ?></td>
-                
-                $this_tot_Al_b1_r1 = $this_df_1 * $this_sample['ICP_Al_split1_run1'] * $this_soln_wt;
-                
-                <td><? printf('%.1f', $tot_al[$a][$s][$r]); ?></td>
-                <td>
-                    <input type=checkbox value="y" name="use_al[]" 
-                        <? if ($use_be[$a][$s][$r] == 'y') { echo "checked"; } ?> >
-                </td>
-            </tr>
-            <? endfor; // runs?>
-            
-        <? endfor; // splits ?>
-        
-        <!-- Row 6: Averages -->
-        <tr>
-            <td colspan=5></td>
-            <td>Average ug:</td>
-            <td colspan="2">
-                <? printf('%.1f', $be_avg[$a]); ?>
-                +/-
-                <?printf('%.1f', $be_sd[$a]); ?>
-            </td>
-            <td></td>
-
-            <td colspan=2>
-                <? printf('%.1f', $al_avg[$a]); ?>
-                +/- 
-                <? printf('%.1f', $al_sd[$a]); ?>
-            </td>
-        </tr>
-
-        <!-- ROW 7. PERCENT UNCERTAINTY -->
-
-        <tr>
-            <td colspan=5></td>
-            <td>Percent error:</td>
-            <td colspan=2><? printf('%.1f', $pct_err_be[$a]); ?></td>
-            <td></td>
-            <td colspan=2><? printf('%.1f', $pct_err_al[$a]); ?></td>
-        </tr>
-
-        <tr>
-            <td colspan=5></td>
-            <td>Pct. recovery:</td>
-            <td colspan=2><? printf('%.1f', $recovery_be[$a]); ?></td>
-            <td></td>
-            <td colspan=2><? printf('%.1f', $recovery_al[$a]); ?></td>
-        </tr>
-
-        <? if ( ($a % 2) == 0): ?>
-            <tr><td colspan=11><hr></td></tr>
-            <tr>
-                <td colspan=11 align=center>
-                    <input type="submit" value="Save and refresh">
-                </td>
-            </tr>
+        <? if ($s == 0): ?>
+            <td><?=$an->id?></td>
+            <td><?=$an->sample_name?></td>
+        <? else: ?>
+            <td colspan="2"></td>
         <? endif; ?>
         
+        <? for ($r = 0; $r < $nruns[$a][$s]; $r++): // ICP run loop
+            $run = &$an->Split[$s]->IcpRun[$r];  ?>
+            
+            <? if ($r == 0):?>
+                <td>Split <?=$an->Split[$s]->split_num?></td>
+                <td><?=$an->Split[$s]->SplitBkr->bkr_number?></td>
+            <? else: ?>
+                <td colspan="4"></td>
+            <? endif; ?>
+            
+            <td>Run <?=$run->run_num?></td>
+            <td><? printf('%.4f', $icp_be[$a][$s][$r]); ?></td>
+            <td><? printf('%.1f', $be_tot[$a][$s][$r]); ?></td>
+            <td>
+                <input type=checkbox value="<?=$run->id?>" name="use_be[]" <?=($run->use_be=="y")?'checked':''?>>
+            </td>
+            <td><? printf('%.4f', $icp_al[$a][$s][$r]); ?></td>
+            <td><? printf('%.1f', $al_tot[$a][$s][$r]); ?></td>
+            <td>
+                <input type=checkbox value="<?=$run->id?>" name="use_al[]" <?=($run->use_al=="y")?'checked':''?>>
+            </td>
+        </tr>
+        <? endfor; // runs?>
+            
+    <? endfor; // splits ?>
+    
+    <!-- Row 6: Averages -->
+    <tr>
+        <td colspan=5></td>
+        <td>Average ug:</td>
+        <td colspan="2">
+            <? printf('%.1f', $be_avg[$a]); ?>
+            +/-
+            <?printf('%.1f', $be_sd[$a]); ?>
+        </td>
+        <td></td>
+
+        <td colspan=2>
+            <? printf('%.1f', $al_avg[$a]); ?>
+            +/- 
+            <? printf('%.1f', $al_sd[$a]); ?>
+        </td>
+    </tr>
+
+    <!-- ROW 7. PERCENT UNCERTAINTY -->
+
+    <tr>
+        <td colspan=5></td>
+        <td>Percent error:</td>
+        <td colspan=2><? printf('%.1f', $be_pct_err[$a]); ?></td>
+        <td></td>
+        <td colspan=2><? printf('%.1f', $al_pct_err[$a]); ?></td>
+    </tr>
+
+    <tr>
+        <td colspan=5></td>
+        <td>Pct. recovery:</td>
+        <td colspan=2><? printf('%.1f', $be_recovery[$a]); ?></td>
+        <td></td>
+        <td colspan=2><? printf('%.1f', $al_recovery[$a]); ?></td>
+    </tr>
+
+    <? if ( ($a % 2) != 0): ?>
+        <tr><td colspan=11><hr></td></tr>
+        <tr>
+            <td colspan=11 align=center>
+                <input type="submit" value="Save and refresh">
+            </td>
+        </tr>
+        <? if ($a != $nsamples - 1) echo $cols; ?>
+    <? endif; ?>
+    
 <? endfor; // analyses ?>
-        
 
 <tr><td colspan=11><hr></td></tr>
 
