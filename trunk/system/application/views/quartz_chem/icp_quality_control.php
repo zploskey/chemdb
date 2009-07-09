63958192374d882326e2b6a8019d4673e5777e03
Zach Ploskey <zploskey@gmail.com>
2009-07-09 10:48:37 +0000

    Major refactoring of the reports and icp_quality control to eliminate redundant calculations. Also moved some repetitive html-generating functionality to models. Deleted some ancient models that used the CI DB layer. After some thought, it's probably best to keep Doctrine on a tag and just keep up on whether there are updates available.
    git-svn-id: svn+ssh://ovid.u.washington.edu/rc22/d09/cosmolab/repos/svn/chem_db@86 b159389c-fc5d-0410-935c-b53355241f03

--- trunk/system/application/views/quartz_chem/icp_quality_control.php
+++ trunk/system/application/views/quartz_chem/icp_quality_control.php
@@ -1,19 +1,19 @@
 <?=form_open('quartz_chem/icp_quality_control', '', 
-    array('batch_id' => $batch->id, 'refresh' => true)) // hidden vars ?>
+    array('batch_id' => $batch['id'], 'refresh' => true)) // hidden vars ?>
 
 <table width=800 class=arial10>
     <tr>
         <td> 
         <h3>Batch information:</p></h3>
-        Batch ID: <?=$batch->id?> <br>
-        Batch start date: <?=$batch->start_date?> <br>
-        Batch owner: <?=$batch->owner?> <br>
-        Batch description: <?=$batch->description?> <br>
+        Batch ID: <?=$batch['id']?> <br>
+        Batch start date: <?=$batch['start_date']?> <br>
+        Batch owner: <?=$batch['owner']?> <br>
+        Batch description: <?=$batch['description']?> <br>
         </td>
     </tr>
     <tr>
         <td colspan=4>Batch notes:<br>
-            <textarea align="center" name="notes" rows="5" cols="100"><?=$batch->notes?></textarea>
+            <textarea align="center" name="notes" rows="5" cols="100"><?=$batch['notes']?></textarea>
         </td>
     </tr>
     <tr><td><hr></td></tr>
@@ -41,39 +41,39 @@ echo $cols = <<<COL
 </tr>
 COL;
 
-for ($a = 0; $a < $nsamples; $a++):
-    $an = &$batch->Analysis[$a]; ?>
+for ($a = 0; $a < $batch['nsamples']; $a++):
+    $an = &$batch['Analysis'][$a]; ?>
     <tr><td colspan="11"><hr></td></tr>
     <tr>    
-    <? for ($s = 0; $s < $nsplits[$a]; $s++): ?>
+    <? for ($s = 0; $s < $an['nsplits']; $s++): ?>
     
         <? if ($s == 0): ?>
-            <td><?=$an->id?></td>
-            <td><?=$an->sample_name?></td>
+            <td><?=$an['id']?></td>
+            <td><?=$an['sample_name']?></td>
         <? else: ?>
             <td colspan="2"></td>
         <? endif; ?>
         
-        <? for ($r = 0; $r < $nruns[$a][$s]; $r++): // ICP run loop
-            $run = &$an->Split[$s]->IcpRun[$r];  ?>
+        <? for ($r = 0; $r < $an['Split'][$s]['nruns']; $r++): // ICP run loop
+            $run = &$an['Split'][$s]['IcpRun'][$r];  ?>
             
             <? if ($r == 0):?>
-                <td>Split <?=$an->Split[$s]->split_num?></td>
-                <td><?=$an->Split[$s]->SplitBkr->bkr_number?></td>
+                <td>Split <?=$an['Split'][$s]['split_num']?></td>
+                <td><?=$an['Split'][$s]['SplitBkr']['bkr_number']?></td>
             <? else: ?>
                 <td colspan="4"></td>
             <? endif; ?>
             
-            <td>Run <?=$run->run_num?></td>
-            <td><? printf('%.4f', $icp_be[$a][$s][$r]); ?></td>
-            <td><? printf('%.1f', $be_tot[$a][$s][$r]); ?></td>
+            <td>Run <?=$run['run_num']?></td>
+            <td><? printf('%.4f', $run['be_result']); ?></td>
+            <td><? printf('%.1f', $run['be_tot']); ?></td>
             <td>
-                <input type=checkbox value="<?=$run->id?>" name="use_be[]" <?=($run->use_be=="y")?'checked':''?>>
+                <input type=checkbox value="<?=$run['id']?>" name="use_be[]" <?=($run['use_be']=="y")?'checked':''?>>
             </td>
-            <td><? printf('%.4f', $icp_al[$a][$s][$r]); ?></td>
-            <td><? printf('%.1f', $al_tot[$a][$s][$r]); ?></td>
+            <td><? printf('%.4f', $run['al_result']); ?></td>
+            <td><? printf('%.1f', $run['al_tot']); ?></td>
             <td>
-                <input type=checkbox value="<?=$run->id?>" name="use_al[]" <?=($run->use_al=="y")?'checked':''?>>
+                <input type=checkbox value="<?=$run['id']?>" name="use_al[]" <?=($run['use_al']=="y")?'checked':''?>>
             </td>
         </tr>
         <? endfor; // runs?>
@@ -85,16 +85,16 @@ for ($a = 0; $a < $nsamples; $a++):
         <td colspan=5></td>
         <td>Average ug:</td>
         <td colspan="2">
-            <? printf('%.1f', $be_avg[$a]); ?>
+            <? printf('%.1f', $an['be_avg']); ?>
             +/-
-            <?printf('%.1f', $be_sd[$a]); ?>
+            <?printf('%.1f', $an['be_sd']); ?>
         </td>
         <td></td>
 
         <td colspan=2>
-            <? printf('%.1f', $al_avg[$a]); ?>
+            <? printf('%.1f', $an['al_avg']); ?>
             +/- 
-            <? printf('%.1f', $al_sd[$a]); ?>
+            <? printf('%.1f', $an['al_sd']); ?>
         </td>
     </tr>
 
@@ -103,17 +103,17 @@ for ($a = 0; $a < $nsamples; $a++):
     <tr>
         <td colspan=5></td>
         <td>Percent error:</td>
-        <td colspan=2><? printf('%.1f', $be_pct_err[$a]); ?></td>
+        <td colspan=2><? printf('%.1f', $an['be_pct_err']); ?></td>
         <td></td>
-        <td colspan=2><? printf('%.1f', $al_pct_err[$a]); ?></td>
+        <td colspan=2><? printf('%.1f', $an['al_pct_err']); ?></td>
     </tr>
 
     <tr>
         <td colspan=5></td>
         <td>Pct. recovery:</td>
-        <td colspan=2><? printf('%.1f', $be_recovery[$a]); ?></td>
+        <td colspan=2><? printf('%.1f', $an['be_recovery']); ?></td>
         <td></td>
-        <td colspan=2><? printf('%.1f', $al_recovery[$a]); ?></td>
+        <td colspan=2><? printf('%.1f', $an['al_recovery']); ?></td>
     </tr>
 
     <? if ( ($a % 2) != 0): ?>
@@ -123,7 +123,7 @@ for ($a = 0; $a < $nsamples; $a++):
                 <input type="submit" value="Save and refresh">
             </td>
         </tr>
-        <? if ($a != $nsamples - 1) echo $cols; ?>
+        <? if ($a != $batch['nsamples'] - 1) echo $cols; ?>
     <? endif; ?>
     
 <? endfor; // analyses ?>
