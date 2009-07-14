2d101d0f98645e46b08d0c9b96600ad28e64fd60
Zach Ploskey <zploskey@gmail.com>
2009-07-14 19:15:07 +0000

    Alchecks add_solution_weights page runs great now.
    git-svn-id: svn+ssh://ovid.u.washington.edu/rc22/d09/cosmolab/repos/svn/chem_db@95 b159389c-fc5d-0410-935c-b53355241f03

--- trunk/system/application/models/AlcheckBatchTable.php
+++ trunk/system/application/models/AlcheckBatchTable.php
@@ -28,4 +28,19 @@ class AlcheckBatchTable extends Doctrine_Table
             ->where('b.description != ?', 'Dummy batch')
             ->execute();
     }
+    
+    /**
+     * Used in Alchecks repeatedly to grab analyses and sample names, sorted ascending.
+     * @return Doctrine_Query that retrieves an AlcheckBatch, analyses and samples (just names).
+     */
+    public function getJoinQuery($batch_id)
+    {
+        return Doctrine_Query::create()
+            ->from('AlcheckBatch b')
+            ->leftJoin('b.AlcheckAnalysis a')
+            ->leftJoin('a.Sample s')
+            ->select('b.*, a.*, s.id, s.name')
+            ->where('b.id = ?', $batch_id)
+            ->orderBy('a.number_within_batch ASC'); 
+    }
 }
