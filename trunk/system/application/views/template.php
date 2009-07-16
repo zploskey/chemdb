a8a7b61b80e21aca676f2f4b9dbeb133f85353e4
Zach Ploskey <zploskey@gmail.com>
2009-07-16 04:13:57 +0000

    Resolving some issues with foreach by reference being broken in PHP. Need to unset the loop variable after the loop if you want to reuse it as a not-by-reference variable.
    git-svn-id: svn+ssh://ovid.u.washington.edu/rc22/d09/cosmolab/repos/svn/chem_db@99 b159389c-fc5d-0410-935c-b53355241f03

--- trunk/system/application/views/template.php
+++ trunk/system/application/views/template.php
@@ -1,28 +1,28 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-	"http://www.w3.org/TR/html4/strict.dtd">
+    "http://www.w3.org/TR/html4/strict.dtd">
 
 <html>
-	<head>
-		<title>UW-CNL-DB -- <?=$title?></title>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-		<base href="<?php echo base_url(); ?>" />
-		<?php echo link_tag('css/style.css'); ?>
-	</head>
+    <head>
+        <title>UW-CNL-DB -- <?=$title?></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <base href="<?php echo base_url(); ?>" />
+        <?php echo link_tag('css/style.css'); ?>
+    </head>
 
-	<body>
-		<div id="wrapper">
-			<div id="header">
-				<?=$this->load->view('header')?>
-			</div>
-			<div id="nav">
-				<?=$this->load->view('navigation')?>
-			</div>
-			<div id="main">
-				<?=$this->load->view($main)?>
-			</div>
-			<div id="footer">
-				<?=$this->load->view('footer')?>
-			</div>
-		</div>
-	</body>
+    <body>
+        <div id="wrapper">
+            <div id="header">
+                <?=$this->load->view('header')?>
+            </div>
+            <div id="nav">
+                <?=$this->load->view('navigation')?>
+            </div>
+            <div id="main">
+                <?=$this->load->view($main)?>
+            </div>
+            <div id="footer">
+                <?=$this->load->view('footer')?>
+            </div>
+        </div>
+    </body>
 </html>
