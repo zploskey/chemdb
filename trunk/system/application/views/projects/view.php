a941adc43457185219ce00eafb46148d481ff8df
Zach Ploskey <zploskey@gmail.com>
2009-07-20 10:41:38 +0000

    Adding autocomplete to sample names in qchem sample loading page. Beginning to link samples and projects in views, still need to add facilities to edit those relationships.
    git-svn-id: svn+ssh://ovid.u.washington.edu/rc22/d09/cosmolab/repos/svn/chem_db@101 b159389c-fc5d-0410-935c-b53355241f03

--- trunk/system/application/views/projects/view.php
+++ trunk/system/application/views/projects/view.php
@@ -1,25 +1,48 @@
 <div id="navbar">
-	<ul>
-		<li><?=anchor('welcome','Return to Main Menu')?> |</li>
-		<li><?=anchor('projects','Projects List')?></li>
-	</ul>
+    <ul>
+        <li><?=anchor('welcome','Return to Main Menu')?> |</li>
+        <li><?=anchor('projects','Projects List')?></li>
+    </ul>
 </div>
-
-<p><h2><?=$subtitle?> (<?=anchor('projects/edit/'.$project->id, 'Edit')?>)</h2></p>
-
-<div id="data">
-	<table>
-		<tr>
-			<td align="right">Name:</td>
-			<td><?=$project->name?></td>
-		</tr>
-		<tr>
-			<td align="right">Date Added:</td>
-			<td><?=$project->date_added?></td>
-		</tr>
-		<tr>
-			<td align="right">Description:</td>
-			<td><?=$project->description?></td>
-		</tr>
-	</table>
+<br>
+<p><h2><?=$subtitle?> (<?=anchor('projects/edit/'.$proj->id, 'Edit')?>)</h2></p>
+<br>
+<div class="data">
+    <table>
+        <tr>
+            <td align="right"><em>Name:</em></td>
+            <td><?=$proj->name?></td>
+        </tr>
+        <tr>
+            <td align="right"><em>Date Added:</em></td>
+            <td><?=$proj->date_added?></td>
+        </tr>
+        <tr>
+            <td align="right"><em>Description:</em></td>
+            <td><?=$proj->description?></td>
+        </tr>
+    </table>
+    <br><br>
+    <div class="data">
+    <p>
+        <table class="itemlist">
+            <tr>
+                <th>Associated Samples</th>
+                <th>Actions</th>
+            </tr>
+            <? foreach ($samples as $s): ?>
+                <tr>
+                    <td><?=$s->name?></td>
+                    <td>
+                        <span id="actionbar">
+                            <ul>
+                                <li><?=anchor('samples/view/'.$s->id, 'View')?></li>
+                                <li><?=anchor('samples/edit/'.$s->id, 'Edit')?></li>
+                            </ul>
+                        </span>
+                    </td>
+                </tr>
+            <? endforeach;?>
+        </table>
+    </p>
 </div>
