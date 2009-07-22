c57c8cfc0550295af88c1bdcc1afcd55f782a9d6
Zach Ploskey <zploskey@gmail.com>
2009-07-22 00:41:49 +0000

    Adding an autocompleted search field to samples page. Tweaked some callbacks.
    git-svn-id: svn+ssh://ovid.u.washington.edu/rc22/d09/cosmolab/repos/svn/chem_db@110 b159389c-fc5d-0410-935c-b53355241f03

--- trunk/system/application/views/samples/index.php
+++ trunk/system/application/views/samples/index.php
@@ -1,42 +1,46 @@
 <div id="navbar">
-	<ul>
-		<li><?=anchor('welcome', 'Return to Main Menu')?></li> |
-		<li><?=anchor('samples/edit','Add Sample')?></li>
-	</ul>
+    <ul>
+        <li><?=anchor('welcome', 'Return to Main Menu')?></li> |
+        <li><?=anchor('samples/edit','Add Sample')?></li>
+    </ul>
 </div>
-
+<br>
+<?=form_open('samples/index')?>
+    Sample search: <input type="text" class="sample_name" name="query">
+<?=form_close()?>
+<br>
 <div class="pagination">
-	<?=$pagination?>
+    <?=$pagination?>
 </div>
 <div class="data">
-	<p>
-		<table class="itemlist">
-			<tr>
-				<th>
-				    <?=anchor("samples/index/$sort_by/$alt_sort_dir/$alt_sort_page", 'Name')?>
-				</th>
-				<th>Actions</th>
-			</tr>
-		
-			<?php foreach($samples as $s): ?>
-		
-				<tr>
-					<td id="name"><?=$s->name?></td>
-					<td>
-						<span id="actionbar">
-							<ul>
-								<li><?=anchor('samples/view/'.$s->id, 'View')?></li>
-								<li><?=anchor('samples/edit/'.$s->id, 'Edit')?></li>
-							</ul>
-						</span>
-					</td>
-				</tr>
-		
-			<?php endforeach; ?>
-		
-		</table>
-	</p>
+    <p>
+        <table class="itemlist">
+            <tr>
+                <th>
+                    <?=anchor("samples/index/$sort_by/$alt_sort_dir/$alt_sort_page", 'Name')?>
+                </th>
+                <th>Actions</th>
+            </tr>
+
+            <?php foreach($samples as $s): ?>
+
+                <tr>
+                    <td id="name"><?=$s->name?></td>
+                    <td>
+                        <span id="actionbar">
+                            <ul>
+                                <li><?=anchor('samples/view/'.$s->id, 'View')?></li>
+                                <li><?=anchor('samples/edit/'.$s->id, 'Edit')?></li>
+                            </ul>
+                        </span>
+                    </td>
+                </tr>
+
+            <?php endforeach; ?>
+
+        </table>
+    </p>
 </div>
 <div class="pagination">
-	<?=$pagination?>
+    <?=$pagination?>
 </div>
