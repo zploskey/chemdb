f0ddb71407ddf138a5e5924eb4880a1a15960651
Zach Ploskey <zploskey@gmail.com>
2009-07-27 20:05:06 +0000

    Fixing previous carrier info lookup. Explicitly sorting our analyses now. Formatting.
    git-svn-id: svn+ssh://ovid.u.washington.edu/rc22/d09/cosmolab/repos/svn/chem_db@112 b159389c-fc5d-0410-935c-b53355241f03

--- trunk/system/application/models/BatchTable.php
+++ trunk/system/application/models/BatchTable.php
@@ -10,14 +10,14 @@ class BatchTable extends Doctrine_Table
             ->leftJoin('b.Analysis a')
             ->leftJoin('a.Split s')
             ->fetchOne();
-        
+
         $count = 0;
         foreach ($batch->Analysis as $a) {
             $count += $batch->Split->count();
         }
         return $count;
     }
-    
+
     /**
      * Fetches a batch object with Al and Be carrier data.
      *
@@ -33,7 +33,7 @@ class BatchTable extends Doctrine_Table
             ->where('b.id = ?', $id)
             ->fetchOne();
     }
-    
+
     /**
      *
      * @return Batch
@@ -47,7 +47,7 @@ class BatchTable extends Doctrine_Table
             ->orderBy('b.start_date desc')
             ->execute();
     }
-    
+
     /**
      *
      * @return Batch
@@ -60,7 +60,7 @@ class BatchTable extends Doctrine_Table
             ->orderBy('b.start_date desc')
             ->execute();
     }
-    
+
     /**
      * Get batch collection for Icp Quality Control Page by the batch's id.
      *
@@ -80,46 +80,50 @@ class BatchTable extends Doctrine_Table
             ->leftJoin('sp.IcpRun r')
             ->leftJoin('sp.SplitBkr spb')
             ->where('b.id = ?', $batch_id)
-            ->orderBy('sp.split_num')
+            ->orderBy('a.id ASC')
+            ->addOrderBy('sp.split_num')
             ->addOrderBy('r.run_num')
             ->limit(1)
             ->fetchOne();
     }
-    
+
     /**
      *
      * @param int $carrier_id
      * @param date $start_date
      * @return Doctrine_Collection
      */
-    public function findPrevBeCarrierWt($batch_id, $carrier_id)
+    public function findPrevBeCarrierWt($carrier_id, $start_date)
     {
         return $this->createQuery('b')
             ->select('b.wt_be_carrier_final, b.start_date')
             ->where('b.be_carrier_id = ?', $carrier_id)
-            ->addWhere('b.id != ?', $batch_id)
+            ->addWhere('b.start_date < ?', $start_date)
             ->orderBy('b.start_date desc')
             ->limit(1)
             ->fetchOne();
     }
-    
+
     /**
-     *
+     * Fetches a Batch object with the final aluminum carrier weight and start
+     * date fields populated. The object will always have a start_date that is
+     * older than the start date of the passed to this function.
+     * 
      * @param int $carrier_id
-     * @param date $start_date
+     * @param string $start_date
      * @return Doctrine_Collection
      */
-    public function findPrevAlCarrierWt($batch_id, $carrier_id)
+    public function findPrevAlCarrierWt($carrier_id, $start_date)
     {
         return $this->createQuery('b')
             ->select('b.wt_al_carrier_final, b.start_date')
             ->where('b.al_carrier_id = ?', $carrier_id)
-            ->addWhere('b.id != ?', $batch_id)
+            ->addWhere('b.start_date < ?', $start_date)
             ->orderBy('b.start_date desc')
             ->limit(1)
             ->fetchOne();
     }
-    
+
     /**
      *
      * @return void
@@ -128,7 +132,7 @@ class BatchTable extends Doctrine_Table
     {
         return $this->findCompleteById($id)->getReportArray($stats);
     }
-    
+
     public function lock($id)
     {
         return Doctrine_Query::create()
