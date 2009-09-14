a8a845ac6da7a628269b19d0d19cbc9c931da9ec
cosmolab <cosmolab@b159389c-fc5d-0410-935c-b53355241f03>
2009-09-14 20:49:41 +0000

    Modified the directory structure to allow us to keep a tag of the current CodeIgniter version in the ci folder and our application in the app folder. This makes updating CI much easier. Generally we'll only need to check for changes to the application folder. Changes to the CI libraries can be updated by changing the version number in the base svn:externals.
    git-svn-id: svn+ssh://ovid.u.washington.edu/rc22/d09/cosmolab/repos/svn/chem_db@159 b159389c-fc5d-0410-935c-b53355241f03

--- trunk/system/application/views/welcome.php
+++
@@ -1,5 +0,0 @@
-<br><br>
-<p><?=anchor('projects', 'Manage Projects')?></p>
-<p><?=anchor('samples', 'Manage Samples')?></p>
-<p><?=anchor('quartz_chem','Quartz Chemistry')?></p>
-<p><?=anchor('alchecks', 'Aluminum Checks')?></p><br><br>
