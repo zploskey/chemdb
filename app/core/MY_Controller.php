7e4e8a0ebc9f448404b589ba4084fb9375b6ae9d
Zach Ploskey <zploskey@uw.edu>
2012-06-25 21:10:34 -0700

    Fixed controller change for CI upgrade.

--- app/core/MY_Controller.php
+++ app/core/MY_Controller.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class MY_Controller extends Controller
+class MY_Controller extends CI_Controller
 {
 
     function __construct()
