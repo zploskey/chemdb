a5adc0a82e2dae935f8853d720a37748002d2c8b
Zach Ploskey <zach@ploskey.com>
2016-01-30 01:32:45 -0800

    Remove double-/ from paths in doctrine cli script.

--- app/doctrine.php
+++ app/doctrine.php
@@ -5,11 +5,11 @@ require_once('config/database.php');
 // Configure Doctrine Cli
 // Normally these are arguments to the cli tasks but if they are set here the arguments will be auto-filled
 $config = array(
-    'data_fixtures_path'  =>  dirname(__FILE__) . DIRECTORY_SEPARATOR . '/fixtures',
-    'models_path'         =>  dirname(__FILE__) . DIRECTORY_SEPARATOR . '/models',
-    'migrations_path'     =>  dirname(__FILE__) . DIRECTORY_SEPARATOR . '/migrations',
-    'sql_path'            =>  dirname(__FILE__) . DIRECTORY_SEPARATOR . '/sql',
-    'yaml_schema_path'    =>  dirname(__FILE__) . DIRECTORY_SEPARATOR . '/schema');
+    'data_fixtures_path'  =>  dirname(__FILE__) . '/fixtures',
+    'models_path'         =>  dirname(__FILE__) . '/models',
+    'migrations_path'     =>  dirname(__FILE__) . '/migrations',
+    'sql_path'            =>  dirname(__FILE__) . '/sql',
+    'yaml_schema_path'    =>  dirname(__FILE__) . '/schema');
 
 $cli = new Doctrine_Cli($config);
 $cli->run($_SERVER['argv']);
