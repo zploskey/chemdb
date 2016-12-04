bb59da13e368cf9b2df7e7578ea066cc8a96188c
Zach Ploskey <zach@ploskey.com>
2016-12-03 21:13:34 -0800

    Change secondary navbars to classes

--- app/views/samples/nav.php
+++ app/views/samples/nav.php
@@ -1,4 +1,4 @@
-<div id="navbar">
+<div class="navbar">
     <ul>
         <li><?php echo anchor('samples', 'Sample List'); ?> |</li>
         <li><?php echo anchor('samples/edit','Add Sample'); ?></li>
