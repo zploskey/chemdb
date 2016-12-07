b2f28cb42eb1ead2baa589642226b202778f9b51
Zach Ploskey <zach@ploskey.com>
2016-12-07 10:05:41 -0800

    Clean up landing page and nav
    Make order consistent.
    Add containers link to landing page.

--- app/views/navigation.php
+++ app/views/navigation.php
@@ -1,11 +1,11 @@
 <div class="navbar">
     <ul>
-        <li><?php echo anchor('', 'Start Page'); ?></li> |
+        <li><?php echo anchor('', 'Home'); ?></li> |
         <li><?php echo anchor('projects', 'Projects'); ?></li> |
         <li><?php echo anchor('samples', 'Samples'); ?></li> |
-        <li><?php echo anchor('alchecks', 'Alchecks'); ?></li> |
-        <li><?php echo anchor('quartz_chem', 'Quartz Chemistry'); ?> |
-        <li><?php echo anchor('containers', 'Containers'); ?>
+        <li><?php echo anchor('containers', 'Containers'); ?> |
+        <li><?php echo anchor('alchecks', 'Al-checks'); ?></li> |
+        <li><?php echo anchor('quartz_chem', 'Quartz Chemistry'); ?>
     </ul>
 </div>
 <hr>
