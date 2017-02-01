7c9040909ddcc5754424b4ca1b96d1b26274e6c7
Zach Ploskey <zach@ploskey.com>
2017-02-01 14:03:44 -0800

    Set Doctrine CLI to use autoloader

--- app/doctrine.php
+++ app/doctrine.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once('config/database.php');
+require '../vendor/autoload.php';
+require 'config/database.php';
 
 // Configure Doctrine Cli
 // Normally these are arguments to the cli tasks but if they are set here the arguments will be auto-filled
@@ -9,7 +10,8 @@ $config = array(
     'models_path'         =>  dirname(__FILE__) . '/models',
     'migrations_path'     =>  dirname(__FILE__) . '/migrations',
     'sql_path'            =>  dirname(__FILE__) . '/sql',
-    'yaml_schema_path'    =>  dirname(__FILE__) . '/schema');
+    'yaml_schema_path'    =>  dirname(__FILE__) . '/schema',
+);
 
 $cli = new Doctrine_Cli($config);
 $cli->run($_SERVER['argv']);
