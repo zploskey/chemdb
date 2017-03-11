39fc4698912f99a712d011125b0ad5e94b4c1d2a
Zach Ploskey <zach@ploskey.com>
2017-03-11 15:29:46 -0800

    Refactor scripts to use common bootstrap

---
+++ app/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+// Pretend that this script was executed from CodeIgniter
+define('BASEPATH', '.');
+chdir(dirname(__FILE__));
+
+require '../vendor/autoload.php';
+require 'config/database.php';
