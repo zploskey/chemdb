6a55b1981430a8e534de6e04a412910bcca494ee
Zach Ploskey <zach@ploskey.com>
2017-03-13 02:23:31 -0700

    Add latlong and shield factor form validation messages

--- app/language/english/form_validation_lang.php
+++ app/language/english/form_validation_lang.php
@@ -1,5 +1,7 @@
 <?php
 
 // Custom form validation messages
-$lang['form_validation_unique'] = "The {field} field is not unique. Please choose another.";
 $lang['form_validation_alpha_dot_dash'] = 'The {field} field may only contain alphanumeric characters, periods, underscores and dashes.';
+$lang['form_validation_latlong'] = 'The {field} must be between -180 and 180.';
+$lang['form_validation_shield_factor'] = 'The {field} must be from 0 to 1.';
+$lang['form_validation_unique'] = 'The {field} field is not unique. Please choose another.';
