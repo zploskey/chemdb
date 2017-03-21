54b92224ba3e13fa717924e21506f04ed7d82a30
Zach Ploskey <zach@ploskey.com>
2017-03-21 14:10:01 -0700

    Use normalize.css for some sane css defaults
    We can use some of the browser compatibility adjustments in normalize.
    
    Note that removing the old autocomplete CSS changed some of the layout
    slightly. Notably, this resulted in additional vertical space between
    some paragraphs and unordered lists and their surrounding elements. It
    may require some adjustment now that it has been removed. The previous
    autocomplete CSS did a global reset of certain margins, which was
    definitely undesirable.

--- app/views/template.php
+++ app/views/template.php
@@ -6,8 +6,9 @@
         <title>UW-CNL-DB -- <?php echo $title; ?></title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <base href="<?php echo base_url(); ?>" />
-        <?php echo link_tag('css/style.css'); ?>
+        <?php echo link_tag('vendor/components/normalize.css/normalize.css'); ?>
         <?php echo link_tag('vendor/components/jqueryui/themes/base/jquery-ui.css'); ?>
+        <?php echo link_tag('css/style.css'); ?>
         <script type="text/javascript">
         //<![CDATA[
         base_url = '<?php echo base_url(); ?>';
