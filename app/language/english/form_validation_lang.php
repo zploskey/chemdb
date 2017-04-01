eac9cb98416b22eada60b42769bdf15c7b1c5fda
Zach Ploskey <zach@ploskey.com>
2017-03-31 17:59:52 -0700

    Add form validation message for is_numeric

--- app/language/english/form_validation_lang.php
+++ app/language/english/form_validation_lang.php
@@ -2,6 +2,7 @@
 
 // Custom form validation messages
 $lang['form_validation_alpha_dot_dash'] = 'The {field} field may only contain alphanumeric characters, periods, underscores and dashes.';
+$lang['form_validation_is_numeric'] = 'The {field} must be a number.';
 $lang['form_validation_latlong'] = 'The {field} must be between -180 and 180.';
 $lang['form_validation_shield_factor'] = 'The {field} must be from 0 to 1.';
 $lang['form_validation_unique'] = 'The {field} field is not unique. Please choose another.';
