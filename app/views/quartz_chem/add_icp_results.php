4615088a414deb54f3291dd94768accd7e1ffd1c
Zach Ploskey <zach@ploskey.com>
2018-09-29 22:42:47 -0700

    Fix some very dated html in add_icp_results

--- app/views/quartz_chem/add_icp_results.php
+++ app/views/quartz_chem/add_icp_results.php
@@ -1,35 +1,31 @@
 <?php echo form_open('quartz_chem/add_icp_results'); ?>
 <input type="hidden" name="batch_id" value="<?php echo $batch->id; ?>">
-<table width="800" class="arial10">
-    <?php $this->load->view('tr_main_link_hr'); ?>
-    <tr>
-        <td>
-        <h3>Batch information:</p></h3>
-        Batch ID: <?php echo $batch->id; ?> <br>
-        Batch start date: <?php echo $batch->start_date; ?> <br>
-        Batch owner: <?php echo $batch->owner; ?> <br>
-        Batch description: <?php echo $batch->description; ?> <br>
-        </td>
-    </tr>
-    <tr>
-        <td colspan="4">
-            Batch notes:<br>
-            <center>
-            <textarea name="notes" rows="5" cols="100"><?php echo $batch->notes; ?></textarea>
-            </center>
-        </td>
-    </tr>
-    <tr><td><hr></td></tr>
-</table>
+<div id="batch-info">
+  <?php $this->load->view('tr_main_link_hr'); ?>
+  <h3>Batch Information</h3>
+    <hr>
+    Batch ID: <?php echo $batch->id; ?> <br>
+    Batch start date: <?php echo $batch->start_date; ?> <br>
+    Batch owner: <?php echo $batch->owner; ?> <br>
+    Batch description: <?php echo $batch->description; ?> <br>
+    <label for="notes">Batch notes:</label>
+    <textarea id="notes" name="notes" rows="5"><?php echo $batch->notes; ?></textarea>
+    <hr>
+</div>
+<p>Data should be input as:</p>
+<pre>
+Beaker#1 icprun1 run2 run3 ... runN
+Beaker#2 icprun1 run2 run3 ... runN
+</pre>
+<p>For example:</p>
+<pre>
+AB1 0.8363 0.8387 0.8356
+AB2 0.9878 0.9504 0.9764
+</pre>
+<p>This is most easily done by pasting from Excel.</p>
 <p>
-    Data should be input as:<br><br>
-    Beaker#1 icprun1 run2 run3 ... runN<br>
-    Beaker#2 icprun1 run2 run3 ... runN<br><br>
-    i.e.:<br>
-    AB1 0.8363 0.8387 0.8356<br>
-    AB2 0.9878 0.9504 0.9764 <br><br>
-    This is most easily done by pasting from Excel.<br><br>
-    <b>IMPORTANT NOTE:</b> If you make changes here you <em>must</em> redo ICP Quality Control if you want any of the icp results to be ignored in calculations.<br>
+  <b>IMPORTANT NOTE:</b>
+  If you make changes here you <em>must</em> redo ICP Quality Control if you want any of the icp results to be ignored in calculations.
 </p>
 <table width="800">
     <tr>
@@ -42,7 +38,7 @@
     </tr>
 </table>
 <hr>
-<p align="center"><?php echo form_submit('submit', 'Save and refresh'); ?></p>
+<div class="center"><?php echo form_submit('submit', 'Save and refresh'); ?></div>
 <hr>
 <?php echo form_close(); ?>
 
